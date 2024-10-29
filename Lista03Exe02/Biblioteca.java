df1c2947ac1a7098e02938556ec018c959d1d2e8
Eliseu Silva <eueliseeu@gmail.com>
2024-10-29 17:55:35 -0300

    feat: Correção do código

--- Lista03Exe02/Biblioteca.java
+++ Lista03Exe02/Biblioteca.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Biblioteca {
-    private List<Livro> livros;
+    private final List<Livro> livros;
 
     public Biblioteca() {
         livros = new ArrayList<>();
